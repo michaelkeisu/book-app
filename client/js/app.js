6a814509eed19eeb9c5773f8c13569a0c0cd0863
Michael Keisu <michael.keisu@gmail.com>
2014-05-21 17:51:24 +0200

    Added new Book page. Updated design.

--- client/js/app.js
+++ client/js/app.js
@@ -1,20 +1,44 @@
-var app = angular.module('app.bookApp', ['app.rest']);
-// var app = angular.module('app.bookApp', ['app.rest', 'ngRoute']);
+var app = angular.module('app.bookApp', ['ngRoute']);
 
-// app.config(['$routeProvider', function($routeProvider){
-// 	$routeProvider
-// 		.when('/books/', {
-
-// 		})
-// }])
+app.config(['$routeProvider', function($routeProvider){
+	$routeProvider
+		.when('/books/', {
+			templateUrl: 'list.html',
+			controller: 'bookController'
+		})
+		.when('/books/new', {
+			templateUrl: 'new.html',
+			controller: 'newBookController'
+		})
+}])
 
 app.controller('bookController', ['$scope', 'restfulApi', function($scope, restfulApi) {
-	$scope.books = restfulApi.query();
+	restfulApi.list(function(books) {
+		$scope.books = books;
+	});
 	$scope.labels = [ 'Title', 'Author' ];
-
 }]);
 
-angular.module('app.rest', ['ngResource'])
-	.factory('restfulApi', function($resource) {
-		return $resource('http://localhost:3000/books/:id');
-	});
+app.controller('newBookController', ['$scope', '$location', 'restfulApi', function($scope, $location, restfulApi) {
+	$scope.newPost = function() {
+		var book = {
+			title: $scope.title, 
+			author: $scope.author
+		}
+		restfulApi.new(book, function() {
+			$location.path('/books/');
+		})
+	}
+}]);
+
+
+app.factory('restfulApi', ['$http', function($http) {
+	return {
+		'list': function(callback) {
+			$http.get('/books/').success(callback);
+		},
+		'new': function(book, callback) {
+			$http.post('/books/', { 'title': book.title, 'author': book.author }).success(callback);
+		}
+	}
+}]);
